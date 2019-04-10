00198c9d6226ff3ef4eb8f45f317d1f0cad4e614
Nathan Adams <nathanadams123@gmail.com>
2019-04-10 07:43:45 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAAvNhKKtjDkBpdjCMCiMGbXUw_qyhKQjM',
+    authDomain: 'bot-logger.firebaseapp.com',
+    databaseURL: 'https://bot-logger.firebaseio.com',
+    projectId: 'bot-logger',
+    storageBucket: 'bot-logger.appspot.com',
+    messagingSenderId: '97614011921'
+  }
 };
